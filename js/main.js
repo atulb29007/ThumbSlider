a3d87b3e4c2545de54f1c9f19545ae9b7fa1f554
atulb <atulb@29007@gmail.com>
2016-12-14 16:27:19 +0800

    Updated files

--- js/main.js
+++ js/main.js
@@ -1,44 +1,72 @@
 $(document).ready(function () {
-                   $("nav a").click(function () {
-                      //  var classesClicked = $("this").className;
-                       $("nav a").removeClass("active");
-                       $(this).addClass("active");
-                   });
-               });
-$(document).ready(function () {
-  $("#slider a").click(function () {
-    $("#slider a").removeClass("active");
-      $(this).addClass("active");
-    });
+
+  var currentThumb = function (listSelector){
+    var numberItems = $(listSelector).size();
+    var classes;
+    for (var i=0; i< numberItems; i++){
+      if (!($("li").eq(i).hasClass("notOn"))){
+        return i;
+        break;
+      }
+    }
+  }
+
+  var setBackground = function (givenIndex){
+    //Deactivating current photo
+    var currentOn = currentThumb("#thumblist>li");
+    $("li").eq(currentOn).addClass("notOn");
+    //Setting given photo
+    $("li").eq(givenIndex).removeClass("notOn");
+    var backgroundImage = "url('img/slides/photo"+givenIndex+".png')";
+    $(".imageDiv").css('background-image',backgroundImage);
+  }
+
+  var clickBackground = function (directionIn){
+    var currentOn = currentThumb("#thumblist>li");
+    //determining next index
+    var nextOne = $("#thumblist>li").eq(currentOn).next(".notOn").index();
+    if (nextOne < 0){
+      nextOne = 0;
+    }
+    //determining previous index
+    var prevOne = $("#thumblist>li").eq(currentOn).prev(".notOn").index();
+    if (prevOne < 0){
+      prevOne = $("#thumblist>li").size();
+      prevOne--;
+    }
+    if (directionIn==1){
+      //activating next photo
+      setBackground(nextOne);
+    }
+    else {
+      //activating previous photo
+      setBackground(prevOne);
+    }
+  }
+
+  $("#thumblist>li").click(function() {
+    var clickedIndex = $("li").index(this);
+    //activating clicked photo
+    setBackground(clickedIndex);
+    var nextOne = $(this).next(".notOn").index();
+    var prevOne = $(this).prev(".notOn").index();
+    // console.log(nextOne,prevOne);
   });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(function() {
-  $(".show").on("click", function(e) {
-    e.preventDefault();
-    $(this).parent().toggleClass('rotati')
-    var content = $(this).closest("tr").next().find(".content");
-    $(".content").not(content).slideUp();
-    content.slideToggle();
-    content.toggleClass('rotati')
+  $(".rightSlide>i").click(function() {
+    clickBackground(1);
   });
+  $(".leftSlide>i").click(function() {
+    clickBackground(0);
+  });
+
+  $(document).keydown(function(event){
+    if (event.keyCode == 37) {
+      clickBackground(0);
+    }
+    else if (event.keyCode == 39) {
+      clickBackground(1);
+    }
+  });
+
 });
